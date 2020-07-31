426399fabb3cdfb986b89f8d933a1ba18c606eed
mbeman <mbeman@gmail.com>
2020-07-30 18:47:48 -0700

    Update AmazonAges.c

--- AmazonAges.c
+++ AmazonAges.c
@@ -7,17 +7,16 @@ and print out the number of duplicates for each distinct integer
 #include <stdlib.h>
 
 //Declarations
-//int GetArrayLength(int intArray[]);
+int GetArrayLength(int intArray[], int sizeArray);
 
 int main()
 {
     // int array to test
     int testArray[] = {0, 1, 1, 2, 4, 5, 5, 5};
-
-    int currVal = -1;
+    int currVal = testArray[0];
     int currSum = 0;
     int currIndex = 0;
-    //int testArrayLen = GetArrayLength(testArray);
+    int testArrayLen = GetArrayLength(testArray, sizeof(testArray)/sizeof(testArray[0]));
     //int tempValues[] = {(int) malloc(testArrayLen)};
     //int tempSums[] = {(int) malloc(testArrayLen)};
 
@@ -25,34 +24,41 @@ int main()
     int tempSums[50];
     for (int i = currIndex; i < 8; i++)
     {
-        if (currVal < testArray[i] && currIndex == 0)
-        {
-            currVal = testArray[i];
-        }
-        if (currVal == testArray[i])
-        {
-            currSum++;
-        }
-        else
+        if (currVal < testArray[i])
         {
             tempValues[currIndex] = currVal;
             tempSums[currIndex] = currSum;
-            currSum = 0;
+            currSum = 1;
             currIndex++;
+            currVal = testArray[i];
+        }
+        else
+        {
+            currSum++;
         }
     }
 
-    for (int i = 0; i < currIndex; i++)
+    tempValues[currIndex] = currVal;
+    tempSums[currIndex] = currSum;
+
+    for (int i = 0; i < currIndex + 1; i++)
     {
-        printf("There are %d instances of %d\n", tempSums[i], tempValues[i]);
+        if (tempSums[i] == 1)
+        {
+            printf("There is %d instance of %d\n", tempSums[i], tempValues[i]);
+        }
+        else
+        {
+            printf("There are %d instances of %d\n", tempSums[i], tempValues[i]);
+        }
     }
     //free(tempValues);
     //free(tempSums);
 }
 
-/*
-int GetArrayLength(int intArray[])
+
+int GetArrayLength(int intArray[], int sizeArray)
 {
-    return (int) (sizeof(intArray)/sizeof(intArray[0]));
+    int i = 0;
+    return (int) (sizeArray/sizeof(intArray[0]));
 }
-*/
